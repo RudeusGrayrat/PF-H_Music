268aa0f124f4563c143bfc067614cc2e0d6b981a
RudeusGrayrat <miguelnc2502@gmail.com>
2024-04-14 21:05:34 -0500

    a ver

--- Server/src/controllers/Users/getUsers.js
+++ Server/src/controllers/Users/getUsers.js
@@ -3,26 +3,7 @@ const { Temperaments } = require('../../db');
 const getClients = async (req, res) => {
     try {
 
-        const temperamentsInDB = await Temperaments.findAll();
-        if (temperamentsInDB.length == 0) {
-            const apiResponse = await axios.get(`https://api.thedogapi.com/v1/breeds?apy_key=${YOUR_API_KEY}`);
-            const apiTemperaments = apiResponse.data;
-            const temp = new Set();
-            apiTemperaments.forEach(dog => {
-                dog.temperament?.split(', ').forEach(temperament => temp.add(temperament.trim()));
-            });
-            const uniqueTemperaments = [...temp];
-            const savedTemperaments = await Temperaments.bulkCreate(
-                uniqueTemperaments.map((temperament) => {
-                    return { name: temperament };
-                })
-            );
-            res.status(200).json(savedTemperaments);
-        } else {
-            res.status(200).json(temperamentsInDB);
-
-
-        }
+        
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Error al obtener los tipos de Pokémon' });
