ed2ea969a64d002c8e489fbbd4a526e8b60a2887
HDerwyn <hderwynbdo@gmail.com>
2024-04-19 20:23:29 -0400

    modificacion a Users

--- Server/src/models/Users.js
+++ Server/src/models/Users.js
@@ -34,7 +34,7 @@ module.exports = (sequelize) => {
             defaultValue: 'gratis'
         },
 
-        verificado: {
+        esta_verificado: {
             type: DataTypes.BOOLEAN,
             defaultValue: false,
             allowNull: false,
