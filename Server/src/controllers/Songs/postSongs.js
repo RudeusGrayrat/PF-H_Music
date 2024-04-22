052303cb845a7f8b15f4eb8321d5645b443fa9df
iska1234 <sadhuisaac1@gmail.com>
2024-04-21 22:26:50 -0500

    Methods fixed & added

--- Server/src/controllers/Songs/postSongs.js
+++ Server/src/controllers/Songs/postSongs.js
@@ -1,38 +1,54 @@
-const { Songs } = require('../../db');
+const { Songs, Artists, Genres, Albums } = require('../../db');
 
 const postSongs = async (req, res) => {
     try {
-        const { name, image, pathMusic, ArtistID, GenreID } = req.body;
+        const { name, image, pathMusic, ArtistID, GenreID, AlbumsID } = req.body;
 
         const existingSongs = await Songs.findOne({
             where: {
                 name: name,
                 ArtistID: ArtistID
-            }
+            },
+            include: [
+                { model: Artists, attributes: ['name'] },
+                { model: Genres, attributes: ['name'] },
+                { model: Albums, attributes: ['name'] }
+            ]
         });
 
-        // Si ya existe, enviar un mensaje de error
         if (existingSongs) {
             return res.status(400).json({
                 error: 'Ya existe una canción con ese nombre y artista.'
             });
         }
 
-        // Crear la nueva canción
-        const newSongs = await Songs.create({
+        const newSong = await Songs.create({
             image,
             name,
             pathMusic,
             ArtistID,
-            GenreID
+            GenreID,
+            AlbumsID
         });
 
-        return res.status(200).json(newSongs);
+        const artist = await Artists.findByPk(ArtistID, { attributes: ['name'] });
+        const genre = await Genres.findByPk(GenreID, { attributes: ['name'] });
+        const album = await Albums.findByPk(AlbumsID, { attributes: ['name'] });
+
+        const responseData = {
+            id: newSong.id,
+            image: newSong.image,
+            name: newSong.name,
+            pathMusic: newSong.pathMusic,
+            Artist: artist,
+            Genre: genre,
+            Album: album
+        };
+
+        return res.status(200).json(responseData);
     } catch (error) {
-        // Manejar errores de validación de Sequelize
         console.error(error);
         res.status(500).json({ error: 'Error al crear una nueva canción' });
-
     }
 };
 
--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -2,9 +2,12 @@ const { Router } = require('express');
 
 const getSongs = require("../controllers/Songs/getSongs");
 const getSongById = require("../controllers/Songs/getSongById");
+const getSongByTitle = require("../controllers/Songs/getSongByTitle");
 const postUsers = require('../controllers/Users/postUsers');
 const postSongs = require("../controllers/Songs/postSongs");
 const postGenres = require('../controllers/Genres/postGenres');
+const getArtist = require('../controllers/Artists/getArtists');
+const getGenres = require('../controllers/Genres/getGenres');
 const postArtists = require('../controllers/Artists/postArtists');
 const getUsers = require('../controllers/Users/getUsers');
 const login = require('../controllers/Users/login');
@@ -25,6 +28,10 @@ const router = Router();
 
 router.get("/songs", getSongs);
 router.get("/songs/:idSong", getSongById);
+router.get("/songs/search/:name", getSongByTitle);
+router.get("/artists", getArtist);
+router.get("/genres", getGenres);
+router.get("/albums", getAlbums);
 router.get("/login", login);
 router.get("/users", getUsers);
 router.get("/users/:userId", getUserById);
@@ -33,7 +40,6 @@ router.get("getPlaylist", getPlaylist);
 router.get("getPlaylistDetail", getPlaylistDetail);
 router.get("/verification", verification);
 router.get("/getUserPlaylist", getUserPlaylist);
-router.get("/albums", getAlbums);
 
 router.put("/users/:userId/editPasword", putUser);
 router.put("/verification/changeStatus", changeVerificationStatus);
